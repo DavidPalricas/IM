713694a39a3f8cbffc102635b2b08d8172874797
davidpalricas <davipalricas22@gmail.com>
2024-10-25 22:14:14 +0100

    Conextion made with index.htm but not working perfectly

--- app.py
+++ app.py
@@ -1,35 +1,54 @@
 import assistant as assi
-import websocket
+import tts 
+
+from conextion_config import *
+import conextion_config as config
+
+import asyncio
+
+import websockets
 
 
-HOST = "127.0.0.1:8005/IM/USER1/APP1"
+
+HOST = "127.0.0.1:8005"
+OUTPUT = "127.0.0.1:8000"
+
+mmi_cli_out = None
+send_to_voice = tts.TTS(FusionAdd=f"https://{OUTPUT}/IM/USER1/APPSPEECH").sendToVoice
+
+running = True
 
 
-#assi = assi.Assistant()
+
+async def main():
 
 
-# Funções de manipulador de eventos
-def im1_message_handler(ws, message):
-    print("Mensagem recebida:", message)
-
-def socket_open_handler(ws):
-    print("Conexão aberta")
-
-# Criar conexão com WebSocket
-mmiCli_Out = websocket.WebSocketApp(
-    HOST,
-    on_message=im1_message_handler,
-    on_open=socket_open_handler
-)
+    mmi_cli_out_Add = f"wss://{HOST}/IM/USER1/APP"
 
 
-def main():
-    mmiCli_Out.send("Hello World")
+    ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+   # mmi_cli_out =  config.MMIClientSocket(mmi_cli_out_Add + "APP")
+    #mmi_cli_out.onMessage.on(message_handler)
+    #mmi_cli_out.onOpen.on(socket_open_handler)
+    #await mmi_cli_out.openSocket()
 
 
-mmiCli_Out.run_forever()
+    async with websockets.connect( mmi_cli_out_Add,ssl = ssl_context) as websocket:
+       print("connected Websocket")
+       while running:
+            try:
+                message = await websocket.recv()
+                
+                if message not in ["OK", "RENEW"]:
+                    print(message)
+
+                
+            except Exception as e:
+                print(e)
+          
+
 
 if __name__ == "__main__":
-    main()
-
-#VOICE_MODALITY = "127.0.01.8000"
+    asyncio.get_event_loop().run_until_complete(main())
