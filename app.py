5ace03b7203209ee8778c0b65098164db1a2dc8e
davidpalricas <davipalricas22@gmail.com>
2024-10-25 17:58:11 +0100

    Demo given by prof

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 import assistant as assi
-import keyboard as key
-def main():
+import websocket
 
-    assistant = assi.Assistant()
-    assistant.open("https://www.youtube.com/")
-    
-    running = True
-    while running:
-        if key.is_pressed("q"):
-            assistant.close()
-            running = False
-        elif key.is_pressed("s"):
-            assistant.search("Tiagosvki Improviso")
-    
-    
+
+HOST = "127.0.0.1:8005/IM/USER1/APP1"
+
+
+#assi = assi.Assistant()
+
+
+# Funções de manipulador de eventos
+def im1_message_handler(ws, message):
+    print("Mensagem recebida:", message)
+
+def socket_open_handler(ws):
+    print("Conexão aberta")
+
+# Criar conexão com WebSocket
+mmiCli_Out = websocket.WebSocketApp(
+    HOST,
+    on_message=im1_message_handler,
+    on_open=socket_open_handler
+)
+
+
+def main():
+    mmiCli_Out.send("Hello World")
+
+
+mmiCli_Out.run_forever()
 
 if __name__ == "__main__":
     main()
+
+#VOICE_MODALITY = "127.0.01.8000"
