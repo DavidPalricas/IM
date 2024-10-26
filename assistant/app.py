9a5d924e1614b58926161289a98da0e7df67153f
davidpalricas <davipalricas22@gmail.com>
2024-10-26 16:07:55 +0100

    Comments in the app.py file

--- assistant/app.py
+++ assistant/app.py
@@ -2,30 +2,25 @@ import assistant.assistant as assi
 import assistant.web_app_conextions_files.tts as tts 
 import json
 import xml.etree.ElementTree as ET
-
 from web_app_conextions_files.conextion_config import *
-
-
 import asyncio
-
 import websockets
 
-
-
+# THE HOST constant is the address of the websocket that will be connected , the application will receive information from this websocket
+# That information is captured by the user's microphone in the index.html file of the WebApp
 HOST = "127.0.0.1:8005"
+
+# The OUTPUT constant is the address of the websocket that will be connected , the application will send information to this websocket
 OUTPUT = "127.0.0.1:8000"
 
-mmi_cli_out = None
-send_to_voice = tts.TTS(FusionAdd=f"https://{OUTPUT}/IM/USER1/APPSPEECH").sendToVoice
-
+# The running variable is used to control the application's execution
 running = True
-
-
 
 def nlu_extrator(message):
   """
   Extracts the nlu from the websocket's message
   Parameters: message (str): The message received from the websocket
+  Returns: dict: The nlu extracted from the message(contains the text captured by the microphone, user's intent and entities found)
   """ 
    # Remove the <comand> tag
   comand_tag = ET.fromstring(message).findall(".//command")
@@ -36,39 +31,46 @@ def nlu_extrator(message):
   return {"text": message["text"],"intent":message["intent"]["name"],"entities":message["entities"]}
 
 
+def ignore_certificates():
+  """
+  The ignore_certificates function is responsible for ignoring the web certificates
+  """
+  ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
+  ssl_context.check_hostname = False
+  ssl_context.verify_mode = ssl.CERT_NONE
+
+  return ssl_context
 
 async def main():
+  """
+   The main function is responsible for starting the assistant, and make the conextion to the WebApp's websocket
+   It also is responsible for receiving messages and executing the aplication logic
+  """
+
+  assistant = assi.Assistant(tts.TTS(FusionAdd=f"https://{OUTPUT}/IM/USER1/APPSPEECH").sendToVoice)
+
+  # Websocket url adress
+  mmi_cli_out_Add = f"wss://{HOST}/IM/USER1/APP"
+  
+  ssl_context = ignore_certificates()
 
 
-    mmi_cli_out_Add = f"wss://{HOST}/IM/USER1/APP"
+  async with websockets.connect( mmi_cli_out_Add,ssl = ssl_context) as websocket:
+      print("Connected Websocket")
 
-    ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_NONE
-   # mmi_cli_out =  config.MMIClientSocket(mmi_cli_out_Add + "APP")
-    #mmi_cli_out.onMessage.on(message_handler)
-    #mmi_cli_out.onOpen.on(socket_open_handler)
-    #await mmi_cli_out.openSocket()
+      while running:
+          try:
+            print("Waiting for messages")
+            message = await websocket.recv()
+            
+            if message not in ["OK","RENEW"]:       
+              print("Message received")
 
-
-    async with websockets.connect( mmi_cli_out_Add,ssl = ssl_context) as websocket:
-       print("connected Websocket")
-       #tts.TTS(FusionAdd=f"https://{OUTPUT}/IM/USER1/SPEECH").sendToVoice("boas")
-
-       while running:
-            try:
-                print("waiting message")
-                message = await websocket.recv()
-                
-                if message not in ["OK","RENEW"]:
-                      
-                      nlu = nlu_extrator(message)
-
-                      print(nlu)
-
-                      
-            except Exception as e:
-                            print(e)
+              nlu = nlu_extrator(message)
+              print(nlu)
                     
+          except Exception as e:
+                print(e)
+                      
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
